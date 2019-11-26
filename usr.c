e864612c2937c03087f208807cff4f8b02a808f7
Colby Ackerman <csaz86@umsystem.edu>
2019-11-26 14:46:56 -0600

    attach to PCB array

--- usr.c
+++ usr.c
@@ -22,6 +22,7 @@ int main(int arg, char* argv[]) {
     //Init shared memory pointers
     sem_t* shmSemPtr = initShmSemaphore(SHM_KEY_SEM, shmSemSize, &shmSemID, SHM_USR_FLAGS);
     Clock* shmClockPtr = (Clock*)initSharedMemory(SHM_KEY_CLOCK, shmClockSize, &shmClockID, SHM_USR_FLAGS);
+    PCB* shmPcbPtr = (PCB*)initSharedMemory(SHM_KEY_PCB, shmPcbSize, &shmPcbID, SHM_USR_FLAGS);
 
     int count = 0;
     while(1) {
