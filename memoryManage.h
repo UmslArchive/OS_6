ced197c0bbde08b0d2fdd3d024f5961edec792ad
Colby Ackerman <csaz86@umsystem.edu>
2019-11-30 08:16:40 -0600

    mem manage defines

--- memoryManage.h
+++ memoryManage.h
@@ -7,6 +7,8 @@
 #ifndef MEM_MAN_H
 #define MEM_MAN_H
 
-
+#define TOT_SYS_MEM 262144      //256 * 1024 (256K)
+#define MAX_PROCESS_SIZE 32768  //32 * 1024 (32K)
+#define PAGE_SIZE 1024          //(1K)
 
 #endif
