06d1fea377255c2a2ee80c666f23905ad4073680
Colby Ackerman <csaz86@umsystem.edu>
2019-11-30 06:27:09 -0600

    I want these on hoare

---
+++ debug.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ipcs 
+ps -e | grep "usr"
+ps -e | grep "oss"
---
+++ removeShared.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ipcrm -M 0x66666666
+ipcrm -M 0x99999999
+ipcrm -M 0x77777777
+ipcrm -M 0x88888888
+ipcrm -M 0x10101010
+ipcrm -M 0x11111111
+ipcrm -Q 0x12341234
